1df80d31a7fdbd48c1f57bc1e61efa98198af961
skuldur <siggiskuli@gmail.com>
2017-07-18 22:17:01 +0000

    Major refactoring

--- predict.py
+++ predict.py
@@ -1,71 +1,47 @@
+# -*- coding: utf-8 -*-
 import numpy, sys
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.layers import TimeDistributed
-from keras.layers import Activation
-from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
 
-filename = "short_nameofwind.txt"
-raw_text = open(filename, encoding="utf-8").read()
+from utils import getDataset, defineModel, getText, getInputOutput
 
-#create mapping of unique chars to integers
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-int_to_char = dict((i, c) for i, c in enumerate(chars))
+def predict():
+  raw_text = getText()
+  data = getDataset(raw_text)
+  model = defineModel(data)
 
-n_chars = len(raw_text)
-n_vocab = len(chars)
+  loadWeights(model)
+  generateText(model, raw_text)
 
-print ("Total Characters: ", n_chars)
-print ("Total Vocab: ", n_vocab)
+def loadWeights(model):
+  filename = "weights-improvement-68-1.0938-bigger.hdf5"
+  model.load_weights(filename)
+  model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-# prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
-dataX = []
-dataY = []
-for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
-n_patterns = len(dataX)
-print("Total Patterns: ", n_patterns)
-# reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-# normalize
-X = X / float(n_vocab)
-# one hot encode the output variable
-y = np_utils.to_categorical(dataY)
+def generateText(model, raw_text):
+  chars = sorted(list(set(raw_text)))
+  int_to_char = dict((i, c) for i, c in enumerate(chars))
 
-# define the LSTM model
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-model.add(Dropout(0.3))
-model.add(LSTM(256, return_sequences=True))
-model.add(Dropout(0.3))
-model.add(LSTM(256))
-model.add(Dense(n_vocab))
-model.add(Activation('softmax'))
-filename = "weights-improvement-68-1.0938-bigger.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+  n_chars = len(raw_text)
+  n_vocab = len(chars)
 
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print("Seed:")
-print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+  # prepare the dataset of input to output pairs encoded as integers
+  sequence_length = 100
 
-for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
-print("\nDone.")
+  input, output = getInputOutput(raw_text, n_chars, char_to_int, sequence_length)
+
+  start = numpy.random.randint(0, len(input)-1)
+  pattern = input[start]
+
+  for i in range(1000):
+  	x = numpy.reshape(pattern, (1, len(pattern), 1))
+  	x = x / float(n_vocab)
+  	prediction = model.predict(x, verbose=0)
+  	index = numpy.argmax(prediction)
+  	result = int_to_char[index]
+  	seq_in = [int_to_char[value] for value in pattern]
+  	sys.stdout.write(result)
+  	pattern.append(index)
+  	pattern = pattern[1:len(pattern)]
+  print("\nDone.")
+
+if __name__ == "__main__":
+    predict()
---
+++ utils.py
@@ -0,0 +1,70 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import TimeDistributed
+from keras.layers import Activation
+from keras.utils import np_utils
+
+def getText():
+  filename = "short_nameofwind.txt"
+  raw_text = open(filename, encoding="utf8").read()
+  return raw_text
+
+#create mapping of unique chars to integers
+def getDataset(raw_text):
+  chars = sorted(list(set(raw_text)))
+  char_to_int = dict((char, number) for number, char in enumerate(chars))
+
+  n_chars = len(raw_text)
+  n_vocab = len(chars)
+
+  print("Total Characters: ", n_chars)
+  print("Total Vocab: ", n_vocab)
+
+  # prepare the dataset of input to output pairs encoded as integers
+  sequence_length = 100
+
+  dataInput, dataOutput = getInputOutput(raw_text, n_chars, char_to_int, sequence_length)
+
+  n_patterns = len(dataInput)
+  print("Total Patterns: ", n_patterns)
+
+  #reshape X to be [samples, time steps, features]
+  input = numpy.reshape(dataInput, (n_patterns, sequence_length, 1))
+  #normalize
+  input = input / float(n_vocab)
+  #one hot encode the output variable
+  output = np_utils.to_categorical(dataOutput)
+
+  return { 'vocab': n_vocab, 'input': input, 'output': output };
+
+def getInputOutput(text, n_chars, char_to_int, sequence_length):
+  input = []
+  output = []
+  for i in range(0, n_chars - sequence_length, 1):
+    sequence_in = text[i:i + sequence_length]
+    sequence_out = text[i + sequence_length]
+    input.append([char_to_int[char] for char in sequence_in])
+    output.append(char_to_int[sequence_out])
+
+  return input, output
+
+
+def defineModel(data):
+  input = data['input']
+  n_vocab = data['vocab']
+
+  model = Sequential()
+  model.add(LSTM(256, input_shape=(input.shape[1], input.shape[2]), return_sequences=True))
+  model.add(Dropout(0.3))
+  model.add(LSTM(256, return_sequences=True))
+  model.add(Dropout(0.3))
+  model.add(LSTM(256))
+  model.add(Dense(n_vocab))
+  model.add(Activation('softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+  return model
+  
