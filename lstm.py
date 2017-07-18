d33bf941baa194e4a66c8c2ace7c7faa4752c7e1
skuldur <siggiskuli@gmail.com>
2017-07-18 21:31:17 +0000

    refactored code

--- lstm.py
+++ lstm.py
@@ -8,53 +8,84 @@ from keras.layers import Activation
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
-LAYER_NUMBER = 3
+def init():
+	raw_text = getText()
+	data = getDataset(raw_text)
+	model = defineModel(data)
+	fitModel(model, data)
 
-filename = "short_nameofwind.txt"
-raw_text = open(filename, encoding="utf8").read()
+def getText():
+	filename = "short_nameofwind.txt"
+	raw_text = open(filename, encoding="utf8").read()
+	return raw_text
 
 #create mapping of unique chars to integers
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
+def getDataset(raw_text):
+	chars = sorted(list(set(raw_text)))
+	char_to_int = dict((char, number) for number, char in enumerate(chars))
 
-n_chars = len(raw_text)
-n_vocab = len(chars)
+	n_chars = len(raw_text)
+	n_vocab = len(chars)
 
-print("Total Characters: ", n_chars)
-print("Total Vocab: ", n_vocab)
+	print("Total Characters: ", n_chars)
+	print("Total Vocab: ", n_vocab)
 
-# prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
-dataX = []
-dataY = []
-for i in range(0, n_chars - seq_length, 1):
-	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
-n_patterns = len(dataX)
-print("Total Patterns: ", n_patterns)
+	# prepare the dataset of input to output pairs encoded as integers
+	sequence_length = 100
 
-#reshape X to be [samples, time steps, features]
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
-#normalize
-X = X / float(n_vocab)
-#one hot encode the output variable
-y = np_utils.to_categorical(dataY)
+	dataInput, dataOutput = getInputOutput(raw_text, n_chars, char_to_int, sequence_length)
 
-# define the LSTM model
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-model.add(Dropout(0.3))
-model.add(LSTM(256, return_sequences=True))
-model.add(Dropout(0.3))
-model.add(LSTM(256))
-model.add(Dense(n_vocab))
-model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-# define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
-# fit the model
-model.fit(X, y, nb_epoch=100, batch_size=64, callbacks=callbacks_list)
+	n_patterns = len(dataInput)
+	print("Total Patterns: ", n_patterns)
+
+	#reshape X to be [samples, time steps, features]
+	input = numpy.reshape(dataInput, (n_patterns, sequence_length, 1))
+	#normalize
+	input = input / float(n_vocab)
+	#one hot encode the output variable
+	output = np_utils.to_categorical(dataOutput)
+
+	return { 'vocab': n_vocab, 'input': input, 'output': output };
+
+def getInputOutput(text, n_chars, char_to_int, sequence_length):
+	input = []
+	output = []
+	for i in range(0, n_chars - sequence_length, 1):
+		sequence_in = text[i:i + sequence_length]
+		sequence_out = text[i + sequence_length]
+		input.append([char_to_int[char] for char in sequence_in])
+		output.append(char_to_int[sequence_out])
+
+	return input, output
+
+
+def defineModel(data):
+	input = data['input']
+	n_vocab = data['vocab']
+
+	model = Sequential()
+	model.add(LSTM(256, input_shape=(input.shape[1], input.shape[2]), return_sequences=True))
+	model.add(Dropout(0.3))
+	model.add(LSTM(512, return_sequences=True))
+	model.add(Dropout(0.3))
+	model.add(LSTM(512, return_sequences=True))
+	model.add(Dropout(0.3))
+	model.add(LSTM(256))
+	model.add(Dense(n_vocab))
+	model.add(Activation('softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+	return model
+
+
+def fitModel(model, data):
+	filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+	checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
+	callbacks_list = [checkpoint]
+
+	# fit the model
+	model.fit(data['input'], data['output'], nb_epoch=100, batch_size=64, callbacks=callbacks_list)
+    
+
+if __name__ == "__main__":
+    init()
